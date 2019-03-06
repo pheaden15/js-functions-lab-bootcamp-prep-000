a22b1f47159383ab5d8cf473df088caf5d7031be
Phillip Headen <pheaden15@gmail.com>
2019-03-06 02:31:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,3 @@ function happyHolidays()
 {
  return("Happy holidays!") 
 }
-
-function happyHolidaysTo(name)
-{
-  return 'Happy holidays, ${name}!'
-}
-
-function happyHolidaysTo(holiday, name)
-{
-  return 'Happy ${holiday}, ${name}!'
-}
